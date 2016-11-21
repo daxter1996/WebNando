92015b57bff6f134f21cc4826e540cc3164f90a0
Jaume007 <jponsmorillas@gmail.com>
2016-11-21 20:46:34 +0100

    header

---
+++ blue/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<header>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="index.php" class="brand-logo center"><img src="img/logoh.jpg" width="30%"></a>
+            <ul id="nav-mobile" class="right ">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<body>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+<footer>
+
+</footer>
+</html>
