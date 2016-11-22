05de9c43756bb411f273b97cad2e8e1c91e47c5b
daxter1996 <jesuslizalles@gmail.com>
2016-11-22 08:50:45 +0100

    Banderetes a nes header, footer i classe nova a css.

--- blue/bluebeach.php
+++ blue/bluebeach.php
@@ -4,19 +4,27 @@
     <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
-
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <header>
-   <?php include_once "header.html"; ?>
+    <?php include_once "header.html"; ?>
 </header>
 <body>
+<div class="parallax-container">
+    <div class="parallax"><img src="img/blue_beach/IMG_9167.jpg" ></div>
+</div>
 
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
+</script>
+
 </body>
 <?php include_once "footer.php"; ?>
 </html>
