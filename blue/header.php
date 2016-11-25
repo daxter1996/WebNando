d42f02e956f5bcd4ac2892799f14bb9a2bf5b703
Jaume007 <jponsmorillas@gmail.com>
2016-11-25 18:08:19 +0100

    Homepage

--- blue/header.php
+++ blue/header.php
@@ -3,13 +3,14 @@
         <a href="index.php" class="brand-logo left"><img src="img/logoh.jpg" style="height: 70px;"></a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul id="nav-mobile" class="right ">
-            <li><a href="<?php echo $thispage?>" target="_self" onclick="updatecookie("es")"><img class="img-navegacio" src="img/esp.png"/> </a></li>
-            <li><a href="<?php echo $thispage?>" target="_self" onclick="<?php setcookie("langcookie","en");?>"><img class="img-navegacio" src="img/eng.jpg"/></a></li>
+            <li><a href="<?php echo $thispage?>" target="_self" onclick="updatecookie('es')"><img class="img-navegacio" src="img/esp.png"/> </a></li>
+            <li><a href="<?php echo $thispage?>" target="_self" onclick="updatecookie('en')"><img class="img-navegacio" src="img/eng.jpg"/></a></li>
         </ul>
     </div>
     <script>
         function updatecookie(lang) {
-            document.cookie("langcookie=lang");
+            if(lang=="es") document.cookie="langcookie=es";
+            else document.cookie="langcookie=en";
         }
     </script>
 </nav>
