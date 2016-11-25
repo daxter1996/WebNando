af83c87bc5ffa18e311c178ba8b830652e39fcdd
Jaume007 <jponsmorillas@gmail.com>
2016-11-25 17:29:45 +0100

    Homepage

--- blue/header.php
+++ blue/header.php
@@ -1,5 +1,5 @@
 <nav>
-    <div class="nav-wrapper blue">
+    <div class="nav-wrapper blue" style="box-shadow: 0px 15px 37px -2px rgba(0,0,0,0.3);">
         <a href="index.php" class="brand-logo left"><img src="img/logoh.jpg" style="height: 70px;"></a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul id="nav-mobile" class="right ">
