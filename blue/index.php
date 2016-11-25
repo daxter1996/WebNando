f9aca908d58e6a62961e9f655a23a6d58edb0cef
Jaume007 <jponsmorillas@gmail.com>
2016-11-25 18:17:36 +0100

    Homepage

--- blue/index.php
+++ blue/index.php
@@ -16,8 +16,7 @@ if(empty($_COOKIE["langcookie"])){ setcookie("langcookie","es");
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <header>
-    <?php $thispage="index.php";
-   ?>
+    <?php $thispage="index.php";?>
    <?php include_once "header.php"; ?>
 </header>
 <body>
