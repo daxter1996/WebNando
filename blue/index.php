0146bce30696e29ee12980c845ece181106a296e
daxter1996 <jesuslizalles@gmail.com>
2016-12-19 17:55:27 +0100

    Homepage

--- blue/index.php
+++ blue/index.php
@@ -23,49 +23,36 @@ if(empty($_COOKIE["langcookie"])){ setcookie("langcookie","es");
 </header>
 <body>
 <br/>
-<div class="row">
-    <div class="col s12 m0">
-        <img src="img/logo-grande.png" class="responsive-img" style="border-radius: 3px">
-    </div>
-    <div class="col s12 m4 offset-l1m" style="padding: 10px;" >
-        <h4>Blue Beach</h4>
-        <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient because I require little markup to use effectively.</p>
-        <p>I am a very simple card. I am good at containing small bits of information.
-            am convenient because I require little markup to use effectively.</p>
-        <hr>
-        <strong><a href="bluebeach.php" class="bold black-text">MAS INFORMACIÓN</a></strong>
-    </div>
+<div class="imageFons" style="min-height: 900px">
+    <div class="row">
+        <div class="col s12 m0">
+            <img src="img/logo-grande.png" class="responsive-img" style="border-radius: 3px">
+        </div>
+        <div class="col s12 m4 offset-l1m" style="padding: 10px;" >
+            <h4>Blue Beach</h4>
+            <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            <p>I am a very simple card. I am good at containing small bits of information.
+                am convenient because I require little markup to use effectively.</p>
+            <hr>
+            <strong><a href="bluebeach.php" class="bold black-text">MAS INFORMACIÓN</a></strong>
+        </div>
 
-    <div class="col s0 m3">
-        <img src="img/logo-grande-low.png" class="responsive-img" style="border-radius: 3px">
-    </div>
+        <div class="col s0 m3">
+            <img src="img/logo-grande-low.png" class="responsive-img" style="border-radius: 3px">
+        </div>
 
-    <div class="col s12 m4 " style="padding: 10px;">
-        <h4>Blue Beach 2</h4>
-        <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient because I require little markup to use effectively.</p>
-        <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient because I require little markup to use effectively.</p>
-        <hr>
-        <strong><a href="bluebeach2.php" class="bold black-text">MAS INFORMACIÓN</a></strong>
+        <div class="col s12 m4 " style="padding: 10px;">
+            <h4>Blue Beach 2</h4>
+            <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            <hr>
+            <strong><a href="bluebeach2.php" class="bold black-text">MAS INFORMACIÓN</a></strong>
+        </div>
     </div>
 </div>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
