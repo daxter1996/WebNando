f3e987c0262c8da65a759ef96ce901149500eae5
Jesus <jesuslizalles@gmail.com>
2016-12-19 10:04:58 +0100

    Homepage

--- blue/index.php
+++ blue/index.php
@@ -34,11 +34,11 @@ if(empty($_COOKIE["langcookie"])){ setcookie("langcookie","es");
         <p>I am a very simple card. I am good at containing small bits of information.
             am convenient because I require little markup to use effectively.</p>
         <hr>
-        <strong><a href="bluebeach.php" class="bold">MAS INFORMACIÓN</a></strong>
+        <strong><a href="bluebeach.php" class="bold black-text">MAS INFORMACIÓN</a></strong>
     </div>
 
     <div class="col s0 m3">
-        <img src="img/logo-grande.png" class="responsive-img" style="border-radius: 3px">
+        <img src="img/logo-grande-low.png" class="responsive-img" style="border-radius: 3px">
     </div>
 
     <div class="col s12 m4 " style="padding: 10px;">
@@ -48,9 +48,14 @@ if(empty($_COOKIE["langcookie"])){ setcookie("langcookie","es");
         <p>I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively.</p>
         <hr>
-        <strong><a href="bluebeach2.php" class="bold">MAS INFORMACIÓN</a></strong>
+        <strong><a href="bluebeach2.php" class="bold black-text">MAS INFORMACIÓN</a></strong>
     </div>
 </div>
+<br>
+<br>
+<br>
+<br>
+<br>
 <br>
 <br>
 <br>
