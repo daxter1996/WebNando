0f2a5fa6f1ecd8f66bc681bd73480a517169fcdf
daxter1996 <jesuslizalles@gmail.com>
2016-12-20 16:32:01 +0100

    Homepage

--- blue/index.php
+++ blue/index.php
@@ -23,28 +23,32 @@ if(empty($_COOKIE["langcookie"])){ setcookie("langcookie","es");
 </header>
 <body>
 <div class="imageFons" style="min-height: 800px; height: 100%;">
-    <div class="row transparenciaFons degradatPrincipal">
-        <div class="col s12 m0">
-            <img src="img/logo-grande.png" class="responsive-img" style="border-radius: 3px">
-        </div>
-        <div class="col s12 m4 offset-l1m flow-text" style="padding: 10px; font-weight: 500" >
-            <h4><strong class="blueLogo-text text-shadow">Apartamentos Blue Beach</strong></h4>
-            <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-            <hr>
-            <strong><a href="bluebeach.php" class="blueDarken-text text-shadow">MAS INFORMACIÓN</a></strong>
-        </div>
+    <div style="">
+        <div class="row transparenciaFons degradatPrincipal center">
+            <div class="col s12 m0">
+                <img src="img/logo-grande.png" class="responsive-img" style="border-radius: 3px">
+            </div>
+            <div class="col s12 m4 white-text offset-l1m flow-text" style="padding: 10px; font-weight: 500" >
+                <h4><strong class="blueLogo-text text-shadow">Blue Beach</strong></h4>
+                <strong class="blueLogo-text text-shadow flow-text" style="font-weight: 500;">Apartamentos</strong>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                <hr>
+                <a href="bluebeach.php" class="white-text text-shadow" style="font-weight: 400">MAS INFORMACIÓN</a>
+            </div>
 
-        <div class="col s0 m3">
-            <img src="img/logo-grande-low.png" class="responsive-img" style="border-radius: 3px">
-        </div>
+            <div class="col s0 m3">
+                <img src="img/logo-grande-low.png" class="responsive-img" style="border-radius: 3px">
+            </div>
 
-        <div class="col s12 m4 flow-text" style="padding: 10px;font-weight: 500">
-            <h4><strong class="blueLogo-text text-shadow">Apartamentos Blue Beach 2</strong></h4>
-            <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-            <hr>
-            <strong><a href="bluebeach2.php" class="blueDarken-text text-shadow">MAS INFORMACIÓN</a></strong>
+            <div class="col s12 m4 white-text flow-text" style="padding: 10px;font-weight: 500">
+                <h4><strong class="blueLogo-text text-shadow">Blue Beach 2</strong></h4>
+                <strong class="blueLogo-text text-shadow flow-text" style="font-weight: 500;">Apartamentos</strong>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                <hr>
+                <strong><a href="bluebeach2.php" class="white-text text-shadow">MAS INFORMACIÓN</a></strong>
+            </div>
         </div>
     </div>
 </div>
