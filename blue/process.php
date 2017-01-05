20f7dda9102ccc6f0c1c32621211e2c2bfbca007
Jaume007 <jponsmorillas@gmail.com>
2017-01-05 11:57:56 +0100

    Homepage

--- blue/process.php
+++ blue/process.php
@@ -39,7 +39,7 @@ function sendMail1(){
         Nova Solicitud de Reserva
     </div>
     <div class="logo" style="margin: auto;padding: 2%;">
-        <img src="http://83.52.177.177/web/WebNando/blue/img/logo-grande-low.png" style="width: 50%;margin-left: 15%">
+        <img src="http://83.52.177.177/web/WebNando/blue/img/logo-grande-low.png" style="width: 50%;margin-left: 25%">
     </div>
     <div class="content" style="font-size: 150%;padding: 2%;text-align: center;">
         '.$message.'
