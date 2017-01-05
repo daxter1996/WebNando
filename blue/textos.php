0f784bb8b8a31f6e3794f13eda697f85237589d4
Jesus <jesuslizalles@gmail.com>
2017-01-05 10:49:48 +0100

    Fotos galeria

--- blue/textos.php
+++ blue/textos.php
@@ -4,6 +4,7 @@ $textos = array(
     "homebb2" => array("es" => "Grupo de 7 apartamentos de 2 y 3 habitacines categoría 3 llaves Situado entre Calas Picas, C Blanes y Calan forcat con Jardin y Piscina","en" => "Group of 7 apartments of 2 and 3 rooms category 3 keys Located between Calas Picas, C Blanes and Calan forcat with Garden and Pool", "fr" => "Groupe 7 appartements de 2 et 3 chambres 3 clés catégorie Situé entre Calas Picas, C Blanes et Calan forcat avec jardin et piscine", "it" => "Gruppo 7 appartamenti di 2 e 3 camere da letto 3 chiavi categoria Situato tra Calas Picas, C Blanes e Calan Forcat con giardino e piscina", "de" => "Gruppe 7 Wohnungen von 2 und 3 Schlafzimmer 3 Schlüssel Kategorie liegt zwischen Calas Picas, C Blanes und Calan forcat mit Garten und Pool"),
     "apartamentos" => array("es"=>"Apartamentos", "en" => "Apartments", "fr" => "Appartements", "it" => "Appartamenti", "de" => "Wohnungen"),
     "masinfo" => array("es"=>"Mas Informacion", "en" => "More Information", "fr" => "Plus d'informations", "it" => "Più informazioni", "de" => "Mehr Informationen"),
+    "infobb" => array("es"=>"", "en" => "", "fr" => "", "it" => "", "de" => ""),
 );
 
 
